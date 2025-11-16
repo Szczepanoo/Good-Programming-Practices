a44a2b9f588a9d48d9ac8c7625958d38809a689a
Jakub <jakub.szczepanski02@gmail.com>
2025-11-16 11:56:48 +0100

    Feat: Endpointy: /links, /ratings, /tags [#86c6hx03d[COMPLETE]]

--- Lab2_Python_API/main.py
+++ Lab2_Python_API/main.py
@@ -31,12 +31,12 @@ class Tag:
         self.tag = tag
         self.timestamp = timestamp
 
-# Endpoint root
+
+# Endpointy
 @app.get("/")
 def read_root():
     return {"hello": "world"}
 
-# Endpoint /movies
 @app.get("/movies")
 def get_movies():
     movies: List[Movie] = []
@@ -51,3 +51,49 @@ def get_movies():
             )
             movies.append(movie.__dict__)  # serializacja do dict
     return movies
+
+
+@app.get("/links")
+def get_links():
+    links: List[Link] = []
+    with open("Lab2_Python_API/database/links.csv", newline="", encoding="utf-8") as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            link = Link(
+                movieId=int(row["movieId"]),
+                imdbId=row["imdbId"],
+                tmdbId=row["tmdbId"]
+            )
+            links.append(link.__dict__)
+    return links
+
+
+@app.get("/ratings")
+def get_ratings():
+    ratings: List[Rating] = []
+    with open("Lab2_Python_API/database/ratings.csv", newline="", encoding="utf-8") as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            rating = Rating(
+                userId=int(row["userId"]),
+                movieId=int(row["movieId"]),
+                rating=float(row["rating"]),
+                timestamp=int(row["timestamp"])
+            )
+            ratings.append(rating.__dict__)
+    return ratings
+
+@app.get("/tags")
+def get_tags():
+    tags: List[Tag] = []
+    with open("Lab2_Python_API/database/tags.csv", newline="", encoding="utf-8") as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            tag = Tag(
+                userId=int(row["userId"]),
+                movieId=int(row["movieId"]),
+                tag=row["tag"],
+                timestamp=int(row["timestamp"])
+            )
+            tags.append(tag.__dict__)
+    return tags
